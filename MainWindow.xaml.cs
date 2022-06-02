e93b99072dd603295e8edb74b5fa546c2e492442
tyustay <darkfodal2002@gmail.com>
2022-06-02 21:00:00 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace bio
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+       
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            arbitrary.IsEnabled = true;
+        }
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            arbitrary.IsEnabled = false;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            List<operation> biorhythms = new List<operation>();
+            int arbitrarys = 0;
+            DateTime birthDate = DateTime.Now;
+
+            const int phys = 23;
+            const int emot = 28;
+            const int intel = 33;
+
+            try
+            {
+                birthDate = Convert.ToDateTime(BirthDate.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Произошла ошибка при получении даты рождения");
+            }
+
+            if (arbitrary.IsEnabled == true)
+            {
+                try
+                {
+                    arbitrarys = Convert.ToInt32(arbitrary.Text);
+                }
+                catch
+                {
+                    MessageBox.Show("Произошла ошибка при получении данных отчета");
+                }
+            }
+            else
+            {
+                try
+                {
+                    
+                }
+                catch
+                {
+                    MessageBox.Show("Произошла ошибка при получении данных отчета");
+                }
+            }
+            DateTime dateCountDown;
+            for (int i = 0; i < arbitrarys; i++)
+            {
+                dateCountDown = Convert.ToDateTime(countDownTime.Text);
+                var bior = new operation()
+                {
+                    Date = dateCountDown.AddDays(i).ToShortDateString(),
+                    Emotional = Math.Round((Math.Sin(2 * Math.PI * ((dateCountDown - birthDate).Days + i) / emot)) * 100, 2),
+                    Intellectual = Math.Round((Math.Sin(2 * Math.PI * ((dateCountDown - birthDate).Days + i) / intel)) * 100, 2),
+                    Physical = Math.Round((Math.Sin(2 * Math.PI * ((dateCountDown - birthDate).Days + i) / phys)) * 100, 2),
+                };
+                bior.Total = Math.Round(bior.Emotional + bior.Intellectual + bior.Physical, 2);
+
+                biorhythms.Add(bior);
+            }
+            Dates.ItemsSource = biorhythms;
+        }
+    }
+    }
+
+
---
+++ operation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bio
+{
+    class operation
+    {
+        public string Date { get; set; }
+        public double Physical { get; set; }
+        public double Emotional { get; set; }
+        public double Intellectual { get; set; }
+        public double Total { get; set; }
+    }
+}
